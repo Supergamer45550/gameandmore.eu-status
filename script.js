533653e2df727f06849c846766ab13d6fb048bfc
Supergamer45550 <gamespcandmobile93@gmail.com>
2025-06-11 03:49:32 +0200

    changed structure

---
+++ script.js
@@ -0,0 +1,22 @@
+//Script für Pingen der Website
+
+async function checkServer(serverId, url) {
+    try {
+        const response = await fetch(url, { method: 'HEAD', mode: 'no-cors' });
+        document.querySelector(`#${serverId} span`).textContent = "Online";
+        document.querySelector(`#${serverId}`).classList.add("online");
+    } catch (error) {
+        document.querySelector(`#${serverId} span`).textContent = "Offline";
+        document.querySelector(`#${serverId}`).classList.add("offline");
+    }
+}
+
+function updateStatus() {
+checkServer("server1", "https://dev.gameandmore.eu/");
+checkServer("server2", "https://server2.example.com");
+checkServer("status-server", "https://supergamer45550.github.io/gameandmore.eu-status/"); 
+}
+
+
+updateStatus();
+setInterval(updateStatus, 5000); 
